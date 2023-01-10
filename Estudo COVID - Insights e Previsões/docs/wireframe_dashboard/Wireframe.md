4117554dc2b065a0fff38df5774afeeaa81b7dee
aaasatoou <100235862+aaasatoou@users.noreply.github.com>
2023-01-10 20:14:00 -0300

    update - organização

---
+++ Estudo COVID - Insights e Previsões/docs/wireframe_dashboard/Wireframe.md
@@ -0,0 +1,3 @@
+# Esquema Dashboard
+
+Para poder acessar o dashboard, acesse [aqui](https://miro.com/app/board/uXjVP_xXUvk=/?share_link_id=79088215428)
---
+++ Estudo COVID - Insights e Previsões/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Dashboard e modelos preditivos utilizando dados do COVID',
+    author='Emerson Lima, Gustavo Ramos, Lucas Bezerra e Victor Asato',
+    license='MIT',
+)
